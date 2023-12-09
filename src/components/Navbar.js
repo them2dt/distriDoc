83cc59a83274b3dde971dd6fd42e0cee788f5fef
them2dt <mtcreatives@hotmail.com>
2023-12-09 08:11:22 +0100

    create navigation

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import { faBox, faPlus, faWallet } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div className="navigation">
+      <div className="navigation-button logo"></div>
+      <div className="navigation-button">
+        <FontAwesomeIcon icon={faPlus} />
+      </div>
+      <div className="navigation-button">
+        <FontAwesomeIcon icon={faBox} />
+      </div>
+      <div className="navigation-button">
+        <FontAwesomeIcon icon={faWallet} />
+      </div>
+    </div>
+  );
+}
